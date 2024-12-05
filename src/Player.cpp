6f0e0e630b82c9849f3ddb11e5cc1848ac6dd4c8
elias-azar-gh <eliasazar2002@gmail.com>
2024-12-05 10:40:51 +0100

    Added player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,6 +1,10 @@
 #include "Player.h"
+#include <iostream>
+using namespace std;
 
-vector<int> toIntCoord(string coord) {
+<<<<<<< HEAD
+
+Player::vector<int> toIntCoord(string coord) {
 	vector<int> IntCoord;
 	int x = static_cast<int>(coord.at(0)) - 65; //Le code ASCII du A est 65, celui de B est 66, etc.
 	int y = coord.at(1);
@@ -23,21 +27,21 @@ void Player::initShips() {
 		size_t shipLength = shipType.at(1);
 		string shipName{};
 		switch (shipLength) {
-		case 5:
-			shipName = "battleship";
-			break;
-		case 4:
-			shipName = "cruiser";
-			break;
-		case 3:
-			shipName = "destroyer";
-			break;
-		case 2:
-			shipName = "submarine";
-			break;
-		case 1:
-			shipName = "fishing boat";
-			break;
+			case 5:
+				shipName = "battleship";
+				break;
+			case 4:
+				shipName = "cruiser";
+				break;
+			case 3:
+				shipName = "destroyer";
+				break;
+			case 2:
+				shipName = "submarine";
+				break;
+			case 1:
+				shipName = "fishing boat";
+				break;
 		}
 
 		for (size_t shipIndex = 0; shipIndex < NbOfShips; shipIndex++) {
@@ -45,7 +49,7 @@ void Player::initShips() {
 
 			for (size_t cellIndex = 0; cellIndex < shipLength; cellIndex++) {
 
-				//Afficher la grille avec les cellules déjà occupées pour que le joueur ne choisisse pas une cellule déjà occupée
+				Grid.display()
 
 				cout << "Enter coordinates for the " << shipName << " number " << shipIndex << " (" << shipLength - cellIndex << " cell(s) remaining) : ";
 				string coord{};
@@ -53,11 +57,58 @@ void Player::initShips() {
 				vector<int> IntCoord = toIntCoord(coord);
 				int x = IntCoord.at(0);
 				int y = IntCoord.at(1);
-				/*if (!isOccupied(x, y)) {
+				if (!isOccupied(x, y)) {
 					Cell newCell = Cell(x, y);
 					position.push_back(&newCell);
 				}
-				*/
+				
 			}
 		}
 	}
+=======
+using namespace std;
+
+Player* Player::playTurn(const char& row = '_', const char& col = '_') {
+	//Asking confirmation from the player to start his turn
+	cout << "It's " << this->name << "'s turn. Press Enter to start your turn." << endl;
+	cin;
+
+	//Display the player's grid and the opponent's last attack
+	cout << "This is your grid." << endl;
+	if (row != '_') {
+		cout << "Your opponent attacked " << row << col << " on his last turn" << endl;
+	}
+	this->shipGrid.display();
+
+	//Display the player's attack grid
+	cout << "This is the grid of your previous attacks:" << endl;
+	this->attackGrid.display();
+
+	// Ask the player for a cell to attack
+	map<char, int> rowMap = { {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4}, {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9}};
+	char attackRow = '_';
+	int attackCol = -1;
+	while (!rowMap.count(attackRow)) {
+		cout << "Enter a row to attack (Letter between A and J)" << endl;
+		cin >> attackRow; 
+	}
+	while (attackCol < 1 || attackCol > 10) {
+		cout << "Enter a column to attack (Integer between 1 and 10)" << endl;
+		cin >> attackCol;
+	}
+	attackCol--;
+
+	//Update the grids
+	this->attackGrid.attack(rowMap[attackRow], attackCol);
+	this->opponent->shipGrid.attack(rowMap[attackRow], attackCol);
+
+	if (!opponent->hasLost()) {
+		//Start other player's turn
+		return this->opponent->playTurn(attackRow, (char)attackCol);
+	}
+	else {
+		//Return the winner of the game
+		return this;
+	}
+}
+>>>>>>> 19a0e2df728704ecb089925052921f012ccfc7a0
